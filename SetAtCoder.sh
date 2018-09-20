efd4dfac2240b862d3806f49334182713627faef
Oohikata <koki.obi.321@gmail.com>
2018-09-20 20:45:45 +0900

    [Add] Add install comments

--- SetAtCoder.sh
+++ SetAtCoder.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# ==================
-# HOW TO EXECUTE
+# ==================================
+# How To Execute
+# --------------
 # $ bash SetAtCoder.sh
-# ==================
+# 
+# Requirements
+# ------------
+# Below plugins should be installed
+# 	pyenv
+# 	pyenv-virtualenv
+# ==================================
 
-# pyenv and pyenv-virtualenv should be installed
+
+# install python version 3.4.3
 pyenv install 3.4.3
 pyenv virtualenv 3.4.3 atcoder
 pyenv local atcoder
 
 # install library
 pip install numpy scipy scikit-learn
-
 
