daa8d2946344589e836fe19c821ad82914b7674b
Oohikata <koki.obi.321@gmail.com>
2018-09-26 22:04:14 +0900

    [Add] added ipykernel install

--- SetAtCoder.sh
+++ SetAtCoder.sh
@@ -18,8 +18,10 @@ pyenv install 3.4.3
 pyenv virtualenv 3.4.3 atcoder
 pyenv local atcoder
 
+pip install ipython
+python -m ipykernel install --user --name atcoder --display-name "Python (AtCoder)"
+pip install jupyter
+
 # install library
 pip install numpy scipy scikit-learn
-pip install ipython
-pip install jupyter
 
