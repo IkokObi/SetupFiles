6074a32e36f598af318969c437eae41cf6c28988
Oohikata <koki.obi.321@gmail.com>
2018-09-30 01:01:46 +0900

    [Change] changed nlp environment

--- InitialSetup/python_nlp.sh
+++ InitialSetup/python_nlp.sh
@@ -11,6 +11,7 @@
 # - KNP
 # - packages in python_nlp_list.txt 
 # ========================
+Version='anaconda3-5.2.0'
 
 
 
@@ -18,13 +19,7 @@
 if ! type pyenv > /dev/null 2>&1; then
 	brew update
 	brew install pyenv
-	brew install pyenv-virtualenv
 	echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-	echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
-	source ~/.bash_profile
-elif ! type pyenv-virtualenv > /dev/null 2>&1; then
-	git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-	echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
 	source ~/.bash_profile
 fi
 
@@ -65,16 +60,14 @@ fi
 
 # Install specified version of python
 # TensorFlowがpython 3.7.0 に対応していない
-Version='3.6.6'
 if ! pyenv versions | grep ${Version} > /dev/null 2>&1; then
 	pyenv install ${Version}
 fi
 envname='nlp'
 dirname=".tempsetting_${envname}"
-pyenv virtualenv ${Version} ${envname}
 mkdir ${dirname}
 cd ${dirname}
-pyenv local ${envname}
+pyenv local ${Version}
 
 
 # Install library
@@ -86,12 +79,6 @@ for lib in `cat ./../python_${envname}_list.txt`; do
 	echo "========================"
 done
 
-# Install specific kernel
-python -m ipykernel install --user --name ${envname} --display-name "Python (${envname})"
-
-# matplotlib setting
-mpl_path=$(python -c "import matplotlib;print(matplotlib.matplotlib_fname())")
-sed -i -e "/^backend/s/macosx/Tkagg/" ${mpl_path}
 
 # delete temp directory
 cd ..
