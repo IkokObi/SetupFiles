60e26883274989d6d3f9b7dbceda9898a3e8dd01
Oohikata <koki.obi.321@gmail.com>
2018-09-30 20:24:32 +0900

    [Fix] fixed ipython version

--- SetAtCoder.sh
+++ SetAtCoder.sh
@@ -18,8 +18,8 @@ pyenv install 3.4.3
 pyenv virtualenv 3.4.3 atcoder
 pyenv local atcoder
 
-pip install ipython
-pip install jupyter
+pip install ipython==6.5.0
+pip install jupyter==1.0.0
 python -m ipykernel install --user --name atcoder --display-name "Python (AtCoder)"
 # ipykernel is installed during jupyter installation
 
