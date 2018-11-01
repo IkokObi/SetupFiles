18fa56e139d779f049145dff8dd290750fa18cba
Oohikata <koki.obi.321@gmail.com>
2018-11-02 05:46:24 +0900

    [Add] new directory for python environment settings

---
+++ Python_Setup/envset_python.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# ========================
+# Install List
+# ------------
+# - anaconda3-5.2.0
+# - packages in python.txt 
+# ========================
+Version=anaconda3-5.2.0
+
+
+# Install both pyenv and pyenv-virtualenv
+if ! type pyenv > /dev/null 2>&1; then
+	brew update
+	brew install pyenv
+	echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+	source ~/.bash_profile
+fi
+
+
+
+# Install specified version of python
+# TensorFlowがpython 3.7.0 に対応していない
+if ! pyenv versions | grep ${Version} > /dev/null 2>&1; then
+	pyenv install ${Version}
+fi
+dirname=".tempsetting"
+mkdir ${dirname}
+cd ${dirname}
+pyenv local ${Version}
+
+
+# Install library with pip
+echo "===== Installation with pip started ====="
+pip install --upgrade pip
+for lib in `cat ./../pip.txt`; do
+	if [ ${lib:0:1} != "#" ]; then
+		pip install ${lib}
+		echo "========================"
+		echo "Installed ${lib}!"
+		echo "========================"
+	fi
+done
+
+# Install library with conda
+echo "===== Installation with conda started ====="
+for lib in `cat ./../conda.txt`; do
+	if [ ${lib:0:1} != "#" ]; then
+		conda install -c conda-forge ${lib}
+		echo "========================"
+		echo "Installed ${lib}!"
+		echo "========================"
+	fi
+done
+
+# delete temp directory
+cd ..
+rm -rf ${dirname}
+
