8c5fac135c564b8a8e7b9e3be76572d48bf3faea
Obinata(Research) <k.obinata.1238@gmail.com>
2020-08-09 01:08:02 +0900

    add: Add research env script

---
+++ Python_Setup/conda_shellscripts/trial.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -eu
+
+env_name="trial"
+conda create --name ${env_name} python=3.7 \
+       	numpy\
+       	pandas\
+       	matplotlib\
+       	scipy\
+       	jupyter\
+	tqdm\
+	scikit-learn\
+	flake8\
+	black\
+	isort\
+	xlrd\
+	openpyxl\
+	-y
+conda activate ${env_name}
+# conda install -c conda-forge jupyter_contrib_nbextensions -y
+### conda だとnumpy import時に落ちる...
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+
+# conda remove -n trial --all
